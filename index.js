d96a3080b5286509c07f2c5c49dd1b7e6db33019
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-06-20 22:19:03 +0900

    Preserve color on output

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {spawn} = require('child_process');
 
-const git = spawn('git', process.argv.slice(2));
-
-git.stdout.pipe(process.stdout);
-git.stderr.pipe(process.stderr);
+if (process.argv[2] === 'merge' || process.argv[2] === 'pull') {
+  const git = spawn('git', process.argv.slice(2), {stdio: [0, 'pipe', 2]});
+  git.stdout.pipe(process.stdout);
+} else {
+  spawn('git', process.argv.slice(2), {stdio: 'inherit'});
+}
