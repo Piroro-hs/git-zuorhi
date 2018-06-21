32ba7f9edea5ce9c42dd2fdb6b2f48a15877a624
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-06-22 01:00:31 +0900

    Add git-rebase to target

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const readline = require('readline');
 
 const {detect} = require('./conflict');
 
-if (process.argv[2] === 'merge' || process.argv[2] === 'pull') {
+if (
+  process.argv[2] === 'merge' ||
+  process.argv[2] === 'pull' ||
+  (process.argv[2] === 'rebase' &&
+    !process.argv.slice(3).includes('-i') &&
+    !process.argv.slice(3).includes('--interactive'))
+) {
   const git = spawn('git', process.argv.slice(2), {stdio: [0, 'pipe', 2]});
   const rl = readline.createInterface({
     input: git.stdout,
