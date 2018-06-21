10f32cd14c49e223665b507467353c29b9c6609d
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-06-22 00:51:30 +0900

    Re-enable on TTY

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const readline = require('readline');
 
 const {detect} = require('./conflict');
 
-if ((process.argv[2] === 'merge' || process.argv[2] === 'pull') && process.stdout.isTTY) {
+if (process.argv[2] === 'merge' || process.argv[2] === 'pull') {
   const git = spawn('git', process.argv.slice(2), {stdio: [0, 'pipe', 2]});
   const rl = readline.createInterface({
     input: git.stdout,
