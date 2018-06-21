499a1f3403e50200997b8794c95399d8625860a2
Piroro-hs <Piroro-hs@users.noreply.github.com>
2018-06-21 23:53:24 +0900

    Detect  whether TTY or not

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {spawn} = require('child_process');
 
-if (process.argv[2] === 'merge' || process.argv[2] === 'pull') {
+if ((process.argv[2] === 'merge' || process.argv[2] === 'pull') && process.stdout.isTTY) {
   const git = spawn('git', process.argv.slice(2), {stdio: [0, 'pipe', 2]});
   git.stdout.pipe(process.stdout);
 } else {
